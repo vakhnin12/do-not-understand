20e083fbe783a8549115b76b84ddcba223a1dbc6
Serhii <70597660+vakhnin12@users.noreply.github.com>
2021-03-21 12:34:51 +0200

    Update task1.js

--- task1.js
+++ task1.js
@@ -1,46 +1,117 @@
-function createElement(value) {
-    const area = document.createElement("div");
+const names = [
+    "Tomas",
+    "Rodrigo",
+    "Semen",
+    "Umberto"
+];
 
-    area.style.border = "1px solid black";
-    area.style.width = "50px";
-    area.style.height = "50px";
+function createButton(text, classes) {
+    const button = document.createElement("button");
 
+    button.type = "button";
+    button.className = classes;
+    button.innerText = text;
+    button.style = "width: 20%"
 
-    const span = document.createElement("span");
-    let text = value;
-    span.innerText = text;
-
-    area.appendChild(span);
-
-    return area
+    return button;
 };
 
-function createContainer() {
+function createInputField(title, type) {
     const container = document.createElement("div");
-    const area1 = createElement(0);
-    const area2 = createElement(0);
-    const area3 = createElement(0);
-    const area4 = createElement(0);
-    const area5 = createElement(0);
+    container.className = "input-field";
+    container.style = "width: 70%"
+    const span = document.createElement("span");
+    span.innerText = title;
 
-    container.style = "display: flex"
+    const inputElement = document.createElement("input");
+    inputElement.type = type;
+    inputElement.className = "validate";
+    inputElement.value = "";
 
-    container.appendChild(area1);
-    container.appendChild(area2);
-    container.appendChild(area3);
-    container.appendChild(area4);
-    container.appendChild(area5);
 
-    container.addEventListener("click", function (event, value) {
-        value = +event.target.textContent;
-        console.log(value, typeof value);
-        return value + 1;
-    })
-    return container
+
+
+
+    container.appendChild(span);
+    container.appendChild(inputElement);
+
+
+    return { container, inputElement };
 };
 
-const container = createContainer();
 
-document.body.appendChild(container);
+function renderListOfNames(array, parentElement) {
+    parentElement.innerHTML = "";
 
-console.log(container.textContent)
+
+    const ul = document.createElement("ul");
+    ul.className = "names"
+
+    array.forEach(element => {
+
+        const addButtonDel = createButton("Delete", "delete");
+        const addButtonEdit = createButton("Edit", "edit");
+        const buttonsContainer = document.createElement("div");
+        buttonsContainer.appendChild(addButtonDel);
+        buttonsContainer.appendChild(addButtonEdit);
+
+        let li = document.createElement("li");
+        li.className = "name"
+        let span = document.createElement("span")
+        let text = document.createTextNode(element);
+        span.append(text);
+
+        li.append(span);
+        li.appendChild(buttonsContainer);
+        ul.appendChild(li);
+
+        addButtonDel.addEventListener("click", function (event, array) {
+            const deleteName = confirm("delete name?");
+            if (deleteName === true) {
+                ul.removeChild(li);
+                array.splice(element)
+                console.log(element)
+            }
+        })
+
+        addButtonEdit.addEventListener("click", function (event, text) {
+            const editName = prompt("Enter new name");
+            if (editName === null) {
+                return
+            }
+            console.log(li.text)
+            span.innerText = editName;
+        })
+
+    });
+
+    parentElement.appendChild(ul);
+}
+
+function createFormV2(array, parentElement) {
+    const form = document.createElement("div");
+    form.className = "name-form";
+
+    const nameField = createInputField("Name", "text");
+    const addButton = createButton("Add", "add");
+    const list = document.createElement("div");
+
+    form.appendChild(nameField.container);
+    form.appendChild(addButton);
+
+    parentElement.appendChild(form);
+    parentElement.appendChild(list);
+    renderListOfNames(array, list);
+
+    addButton.addEventListener("click", function () {
+        const name = nameField.inputElement.value;
+        array.push(name)
+        renderListOfNames(array, list)
+    });
+}
+
+const cover = document.createElement("div");
+cover.className = "cover";
+createFormV2(names, cover);
+document.body.appendChild(cover);
+
